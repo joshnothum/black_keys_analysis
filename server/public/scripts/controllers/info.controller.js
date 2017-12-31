d857b702061f060d27e9618e1d8ca4ac4da2d016
Josh Nothum <joshnothum@gmail.com>
2017-12-31 10:25:04 -0600

    added word counter

--- server/public/scripts/controllers/info.controller.js
+++ server/public/scripts/controllers/info.controller.js
@@ -9,4 +9,28 @@ myApp.controller('InfoController', function(UserService) {
     UserService.getLyrics();
     
   };
+
+
+  
+  
+  function wordFreq(string) {
+    var words = string.replace(/[.]/g, '').split(/\s/);
+    var freqMap = {};
+    words.forEach(function (w) {
+      if (!freqMap[w]) {
+        freqMap[w] = 0;
+      }
+      freqMap[w] += 1;
+    });
+
+    return freqMap;
+  }
+
+  var freq = wordFreq("I am the big the big bull.");
+  Object.keys(freq).sort().forEach(function (word) {
+    console.log("count of " + word + " is " + freq[word]);
+  });
+
+  console.log(freq);
+  
 });
