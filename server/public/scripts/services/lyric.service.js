4230f8347313b6b5cad40232bc8259864b3fe16e
Josh Nothum <joshnothum@gmail.com>
2018-02-01 20:52:17 -0600

    added for loop to getLyrics function; can process more than one track

--- server/public/scripts/services/lyric.service.js
+++ server/public/scripts/services/lyric.service.js
@@ -3,12 +3,12 @@ myApp.service('LyricService', function ($http, $location) {
     self.black = [];
     self.green = [];
     self.topTen = [];
+    let freqLyric = {};
     //function removes escape characters from lyrics and counts word occurence
     function wordFreq(string) {
         let words = string.toLowerCase().replace(/[.]/g, '').split(/\s/);
-        let freqLyric = {};
+        
         words.forEach(function (lyric) {
-            console.log("lyric", lyric);
             //check for two character or less
             if (lyric.length <= 2) {
             }
@@ -19,23 +19,32 @@ myApp.service('LyricService', function ($http, $location) {
                 freqLyric[lyric] += 1;
             }
         });
-        return freqLyric;
+       
     }
-
-    // function separateLyrics (array){
-    //     for(let i = 0; i < array.length; i++ ){
-    //         wordFreq(array.data[0].lyrics);
-    //     }
-
-    // };
     self.getLyrics = function () {
         return $http.get('/lyric/track').then(function (response) {
-            let freq = wordFreq(response.data[0].lyrics);
-            Object.keys(freq).sort().forEach(function (word) {
+  
+            
+   // for loop to count all the tracks in the data array. Allows count for album
+                for(let i =0; i < response.data.length; i++){
+                    wordFreq(response.data[i].lyrics);
+            }
+
+            
+            // let newThing = Object.values(freqLyric).sort().reverse();
+
+            // self.green = newThing.slice(0, 16);
+
+            // console.log(self.green);
+            // console.log(freqLyric);
+            
+            
+            
+            Object.keys(freqLyric).sort().reverse().slice(0, 16).forEach(function (word) {
                 //push each individual word into array for chart labels in controller
-                self.black.push(word);
+                self.black.push(freqLyric);
                 //push each word occurence count into array for chart data in controller
-                self.green.push(freq[word]);
+                self.green.push(freqLyric[word]);
             });
             return self.black;
         });
