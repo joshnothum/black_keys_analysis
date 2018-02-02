32ce689e4e2562cf3bb430b4898800ff47715f7c
Josh Nothum <joshnothum@gmail.com>
2018-02-01 23:08:21 -0600

    moved randomColor function to service

--- server/public/scripts/controllers/info.controller.js
+++ server/public/scripts/controllers/info.controller.js
@@ -1,25 +1,20 @@
 myApp.controller('InfoController', function (LyricService) {
   console.log('InfoController created');
   var vm = this;
-  vm.lyricService = LyricService;
   let ctx = "myChart";
-  let colorsArray = []; // empty array to store random colors
+
 
   vm.getLyrics = function () {
     LyricService.getLyrics().then(function () {
-      LyricService.black.forEach(function (words) {
-        let getColor = getRandomColor();
-        colorsArray.push(getColor);
-      });
     });
     let myChart = new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: LyricService.black,
+        labels: LyricService.chartLabels,
         datasets: [{
           label: 'Occurences',
-          data: LyricService.green,
-          backgroundColor: colorsArray,
+          data: LyricService.chartValues,
+          backgroundColor: LyricService.colorsArray,
           borderColor: '#ffffff',
           borderWidth: 1
         }]
@@ -35,13 +30,4 @@ myApp.controller('InfoController', function (LyricService) {
       }
     });
   };
-  // getRandom color creates random hex values
-  function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
 });
--- server/public/scripts/services/lyric.service.js
+++ server/public/scripts/services/lyric.service.js
@@ -1,17 +1,18 @@
 myApp.service('LyricService', function ($http, $location) {
-    let self = this;
-    self.black = [];
-    self.green = [];
-    let freqLyric ={};
-    let finalArray = [];
+    const self = this;
+    const freqLyric = {};
+    self.chartLabels = [];
+    self.chartValues = [];
+    self.colorsArray = []; // empty array to store random colors
 
     //function removes escape characters from lyrics and counts word occurence
-    function wordFreq(string) {
-        let words = string.toLowerCase().replace(/[.]/g, '').split(/\s/);
-        
+    function wordFreq(songLyrics) {
+        //set all lyrics to lowerCase to Capital letters do not create duplicates
+        let words = songLyrics.toLowerCase().replace(/[.]/g, '').split(/\s/);
+
         words.forEach(function (lyric) {
-            //check for two character or less
-            if (lyric.length <= 2) {
+            //check for two character or less and 'the
+            if (lyric.length <= 3) {
             }
             else {
                 if (!freqLyric[lyric]) {
@@ -20,30 +21,45 @@ myApp.service('LyricService', function ($http, $location) {
                 freqLyric[lyric] += 1;
             }
         });
-       return freqLyric;
+        return freqLyric;
     }
-
-function sortedArray(freqLyric) {
-    finalWordsArray = Object.keys(freqLyric).map(function(lyric){
-        return {
-            word: lyric,
-            total: freqLyric[lyric]
-        };
-    });
-    finalWordsArray.sort(function(a,b){
-        return b.total - a.total;
-    });
-    return finalWordsArray;
-}
-
+    //function to make an array of objects that are ordered by word occurence
+    function sortedArray(freqLyric) {
+        lyricsArray = Object.keys(freqLyric).map(function (lyric) {
+            //returns object
+            return {
+                word: lyric,
+                total: freqLyric[lyric]
+            };
+        });
+        lyricsArray.sort(function (a, b) {
+            return b.total - a.total;
+        });
+        return lyricsArray;
+    }
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
     self.getLyrics = function () {
         return $http.get('/lyric/track').then(function (response) {
-   // for loop to count all the tracks in the data array. Allows count for album
-            for(let i =0; i < response.data.length; i++){
+            // for loop to count all the tracks in the data array. Allows count for album
+            for (let i = 0; i < response.data.length; i++) {
                 wordFreq(response.data[i].lyrics);
             }
-            sortedArray(freqLyric).slice(0,16);
+            //reduces number in chart to top 15 word occurences with slice method
+            let whiteKeys = sortedArray(freqLyric).slice(0, 16);
+
+            //for loop  to separtate labels and chartValues
+            for (let i = 0; i < whiteKeys.length; i++) {
+                self.chartLabels.push(whiteKeys[i].word);
+                self.colorsArray.push(getRandomColor());
+                self.chartValues.push(whiteKeys[i].total);
+            }
         });
     };
-
 });
